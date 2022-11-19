33441cfc5477eefcfdbd7db0310d994757aebf23
Imamzog <118427519+Imamzog@users.noreply.github.com>
2022-11-19 19:35:48 +0700

    Add files via upload
    UTS SMS 3 PBO Imam maulana 21040033

---
+++ Imam_maulana_21040033.java
@@ -0,0 +1,50 @@
+//Nama  : Imam maulana
+//NIM   : 21040033
+//Kelas : 3F
+package project_uts.DESAIN;
+
+public class Biodata {
+    private String nama,alamat,jk,kls,jurusan;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getJk() {
+        return jk;
+    }
+
+    public void setJk(String jk) {
+        this.jk = jk;
+    }
+
+    public String getKls() {
+        return kls;
+    }
+
+    public void setKls(String kls) {
+        this.kls = kls;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+    
+    
+}
